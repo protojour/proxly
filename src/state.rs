29dc2bcf3fa21e30cdf5a3bf7670e2a48dfde9b5
Audun Halland <audun.halland@pm.me>
2025-03-26 13:21:02 +0100

    chore: upgrade deps

--- src/state.rs
+++ src/state.rs
@@ -1,19 +1,19 @@
 use std::sync::Arc;
 
 use arc_swap::ArcSwap;
-use hickory_resolver::{config::LookupIpStrategy, TokioAsyncResolver};
+use hickory_resolver::{config::LookupIpStrategy, TokioResolver};
 use rustls::{ClientConfig, ServerConfig};
 use tokio_util::sync::CancellationToken;
 
 pub struct ProxlyState {
     pub ingress_tls_config: Arc<ArcSwap<ServerConfig>>,
     pub egress_tls_config: Arc<ArcSwap<ClientConfig>>,
-    pub hickory: hickory_resolver::TokioAsyncResolver,
+    pub hickory: hickory_resolver::TokioResolver,
     pub cancel: CancellationToken,
 }
 
-pub fn new_hickory() -> anyhow::Result<TokioAsyncResolver> {
-    let (config, mut opts) = hickory_resolver::system_conf::read_system_conf()?;
-    opts.ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
-    Ok(TokioAsyncResolver::tokio(config, opts))
+pub fn new_hickory() -> anyhow::Result<TokioResolver> {
+    let mut builder = TokioResolver::builder_tokio()?;
+    builder.options_mut().ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
+    Ok(builder.build())
 }
